34c72777de72cfc40166ee01d7980be0b5fc25d0
Raphael Lullis <raphael@mushroomlabs.com>
2021-01-30 17:00:44 +0100

    Removed unused components

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -6,23 +6,20 @@ import PaperTable from './dataviewers/PaperTable'
 import Button from './buttons/Button'
 
 import Card from './cards/Card'
-import ChartCard from './cards/ChartCard'
 import StatsCard from './cards/StatsCard'
 
 import SidebarPlugin from './layouts/SidebarPlugin/index'
 import ActionPanel from './layouts/ActionPanel'
-
 
 let widgets = {
   ActionPanel,
   FormGroupInput,
   FormGroupSelect,
   Card,
-  ChartCard,
   StatsCard,
   PaperTable,
   DropDown,
-  SidebarPlugin
+  SidebarPlugin,
 }
 
 export default widgets
@@ -32,10 +29,9 @@ export {
   FormGroupInput,
   FormGroupSelect,
   Card,
-  ChartCard,
   StatsCard,
   PaperTable,
   DropDown,
   Button,
-  SidebarPlugin
+  SidebarPlugin,
 }
