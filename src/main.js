06a13179b2ef991ab9ce231f8318324786560dbf
Raphael Lullis <raphael@mushroomlabs.com>
2021-10-08 12:50:33 +0200

    Updated deposit modal dialog and styling. Bump SDK.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 /*!
-=========================================================
-* Vue Paper Dashboard - v2.0.0
-=========================================================
-* Product Page: http://www.creative-tim.com/product/paper-dashboard
-* Copyright 2019 Creative Tim (http://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard/blob/master/LICENSE.md)
-=========================================================
+   =========================================================
+ * Vue Paper Dashboard - v2.0.0
+   =========================================================
+ * Product Page: http://www.creative-tim.com/product/paper-dashboard
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard/blob/master/LICENSE.md)
+   =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
 import Vue from 'vue'
 import App from './App'
@@ -16,8 +16,11 @@ import router from '@/router/index'
 import store from '@/store'
 
 import PaperDashboard from './plugins/paperDashboard'
+import VueClipboard from 'vue-clipboard2'
+
 import 'vue-notifyjs/themes/default.css'
 
+Vue.use(VueClipboard)
 Vue.use(PaperDashboard)
 
 /* eslint-disable no-new */
