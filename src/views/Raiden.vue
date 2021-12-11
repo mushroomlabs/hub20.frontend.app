3e8a561a576b20b1b707baa3a29048fc75723e81
Raphael Lullis <raphael@mushroomlabs.com>
2021-12-11 01:39:05 +0100

    Add section on dashboard

---
+++ src/views/Raiden.vue
@@ -0,0 +1,18 @@
+<template>
+<card v-if="hasAdminAccess" title="Raiden Management">
+
+</card>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: 'raiden',
+  components: {
+  },
+  computed: {
+    ...mapGetters('account', ['hasAdminAccess'])
+
+  }
+}
+</script>
