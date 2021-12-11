1552e0c4e35da41d01d707b7b2fd70618fd25ff2
Raphael Lullis <raphael@mushroomlabs.com>
2021-12-11 03:36:20 +0100

    (feat) Display list of raiden nodes and tables with channel balances

---
+++ src/components/raiden/RaidenChannelTableItem.vue
@@ -0,0 +1,21 @@
+<template>
+<tr>
+  <td class="identifier">{{ channel.identifier }} </td>
+  <td class="network">{{ channel.token.network_id }} </td>
+  <td class="balance">{{ channel.balance | formattedAmount(channel.token) }}</td>
+</tr>
+
+</template>
+<script>
+  import hub20 from 'hub20-vue-sdk'
+export default {
+  filters: {
+    formattedAmount: hub20.filters.formattedAmount
+  },
+  props: {
+    channel: {
+      type: Object
+    }
+  }
+}
+</script>
---
+++ src/components/raiden/RaidenNodeDetail.vue
@@ -0,0 +1,28 @@
+<template>
+<card v-if="hasAdminAccess" class="raiden-node" :title="title">
+  <RaidenChannelTable :raiden="raiden" />
+</card>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+
+import RaidenChannelTable from '@/components/raiden/RaidenChannelTable'
+
+export default {
+  name: 'raiden-node-detail',
+  components: {
+    RaidenChannelTable
+  },
+  props: {
+    raiden: {
+      type: Object
+    },
+  },
+  computed: {
+    ...mapGetters('account', ['hasAdminAccess']),
+    title() {
+      return `Node ${this.raiden.address }`
+    }
+  }
+}
+</script>
